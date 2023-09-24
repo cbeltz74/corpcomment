6bfcf9f151040b488223fa66d6aa063c7ffe594d
Colin Beltz <colinbeltz@gmail.com>
2023-09-24 06:28:23 -0500

    Finished counter component

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+// --- COUNTER COMPONENT ---
 const textareaEl = document.querySelector('.form__textarea');
+const counterEl = document.querySelector('.counter');
+
+const inputHandler = () => {
+    // determine max number of characters.
+    const max = 150;
+    // number of characters that user has currently typed.
+    const numCharTyped = textareaEl.value.length;
+    // calculate number of characters left.
+    const charsLeft = max - numCharTyped;
+    // show number of characters left.
+    counterEl.textContent = charsLeft;
+};  
 
 textareaEl.addEventListener('input', inputHandler);
 
